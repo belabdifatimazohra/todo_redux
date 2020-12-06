6c4883c99bac43f3e22e397106ec516c835bc679
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2020-12-06 15:23:42 +0000

    task listTask

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,36 @@
-import React from 'react'
-
-function Task() {
+import React from "react";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+import { useDispatch } from 'react-redux'
+function Task({ task }) {
+    // dispatch info state
+    const dispatch = useDispatch();
     return (
-        <div>
-            
+        <div
+            className={task.check ? "todo-row complete" : "todo-row"}
+            key={task.id}
+        >
+            <div style={task.check ? {textDecoration:"line-through"}:{}}>
+                {task.task}
+            </div>
+            <div className="icons">
+                <RiCloseCircleLine
+
+                    className="delete-icon"
+                />
+                <TiEdit className="edit-icon"
+                //    onClick={(e) => {
+                //       e.preventDefault();
+                //       dispatch(editList({ id: uuidv4(), task: input, check: false })}}
+                />
+                <input type="checkbox" className="checkBox"
+                    onClick={(e) => {
+                        e.preventDefault(); task.check = true
+                    }} />
+            </div>
+         
         </div>
-    )
+    );
 }
 
-export default Task
+export default Task;
